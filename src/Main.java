5e30be7cec151538d87605c08d98644ffc0caab1
AryaTabani <125027307+AryaTabani@users.noreply.github.com>
2025-05-20 21:29:12 +0330

    Delete src directory

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.HashMap;
-
-public class Main {
-    HashMap<String,Integer> Mymap = new HashMap<>();
-    public static void main(String[] args)
-    {
-        System.out.println("Welcome to this project!");
-    }
-}
